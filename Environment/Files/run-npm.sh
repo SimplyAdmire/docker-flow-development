9bab216adfb5ece276b5e4c510695ec6e9cf434d
Rens Admiraal <rens@simplyadmire.com>
2016-08-15 08:15:22 +0200

    TASK: Add --unsafe-perm to npm

--- Environment/Files/run-npm.sh
+++ Environment/Files/run-npm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "int chown() { return 0; }" > preload.c && gcc -shared -o preload.so preload.c && LD_PRELOAD=$PWD/preload.so npm $@ && rm preload.c preload.so
+echo "int chown() { return 0; }" > preload.c && gcc -shared -o preload.so preload.c && LD_PRELOAD=$PWD/preload.so npm --unsafe-perm $@ && rm preload.c preload.so

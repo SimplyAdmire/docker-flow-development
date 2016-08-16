77e4dac29b4274221d7ddaaa3ab45c11e43533c8
Rens Admiraal <rens@simplyadmire.com>
2016-08-16 14:37:13 +0200

    BUGFIX: Enforce www-data ownership on /var/www

--- bin/full-sync.sh
+++ bin/full-sync.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source ./Environment/bin/load-env.sh
+./Environment/bin/docker-compose.sh exec rsync chown 1000:33 /var/www/
 
 rsync -rlptv --exclude="Data/" --exclude=".git" --delete app/* rsync://127.0.0.1:8${PORTSUFFIX}/volume
--- bin/watch.sh
+++ bin/watch.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+./Environment/bin/docker-compose.sh exec rsync chown 1000:33 /var/www/
+
 fswatch -0 -r -e '.git' -e "app/Data/" -e ".*___$" app | (while read -d "" event; do ./bin/sync.sh ${event}; done)
